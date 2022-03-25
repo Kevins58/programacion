1d06eeb868aaadfb476e10368eeacbabae511241
Kevins58 <102389648+Kevins58@users.noreply.github.com>
2022-03-25 16:24:26 -0500

    Add files via upload

---
+++ tabla re pro.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+main()
+{
+	for(int i= 1; i<=10; i++){
+	cout<<"-------------------"<<endl;
+	cout<<"la tabla del "<< i <<endl;
+	
+	for(int m =0; m <= 10; m++){
+		cout << i <<" x "<< m << " = " <<i*m <<endl;
+	}
+}
+	system("puse");
+	
+	return 0;
+	
+	
+	
+	
+	}
+
+
+	
+	
+	
+	
